75bef3add3a71431f4395b7f3a9c8a64a179c605
zhanghui <zhanghui15@baijiahulian.com>
2021-08-06 15:11:48 +0800

    feat: go basic for

---
+++ cmd/for/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for 循环的多种写法形式
+	i := 1
+	for i < 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
