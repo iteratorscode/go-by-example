b83c7f87313a8a2e7592e9661ab121f70f85a1db
zhanghui <zhanghui15@baijiahulian.com>
2021-08-09 20:28:24 +0800

    feat: go basic non-blocking channel operation

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -54,4 +54,30 @@ func main() {
 	ping(pings, "passed message")
 	pong(pings, pongs)
 	fmt.Println(<-pongs)
+
+	// non blocking channel operation: select -- default
+	messagess := make(chan string)
+	signals := make(chan bool)
+	select {
+	case msg := <-messagess:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+	msgs := "hi"
+	select {
+	// Here msg cannot be sent to the messages channel, because the channel has no buffer and there is no receiver.
+	case messagess <- msgs:
+		fmt.Println("sent message", msgs)
+	default:
+		fmt.Println("no message sent")
+	}
+	select {
+	case msg := <-messagess:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
 }
